e34a37ed18dbe879eac5681a3a9f3efbfbab9c85
João Chaves <jchaaves95@gmail.com>
2023-05-18 14:48:20 -0300

    Preparing sales search navigation vews

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
